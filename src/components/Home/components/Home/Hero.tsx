c068abfbff54dbcd4c3c514bfdb0e32c4685462b
Taha Chtaibi <tchtaibi@e1r11p4.1337.ma>
2023-05-22 17:20:12 +0100

    patch data

--- src/components/Home/components/Home/Hero.tsx
+++ src/components/Home/components/Home/Hero.tsx
@@ -1,17 +1,17 @@
 import GradienBox from '../../../../tools/GradienBox'
 import PlayImg from "../../../../assets/img/Play.svg"
 import "./Hero.scss"
-import { useRecoilValue } from 'recoil';
-import { Mydata } from '../../../../StateManager/StateM'
+import {userType} from '../../../../interface/interfaces'
 import { useSelector } from 'react-redux';
 function Hero() {
-  const data = useRecoilValue(Mydata);
-  // const data = useSelector((state:any) => state.users.users).filter((e:any) => e.admin === true)[0];
+  const users: any = useSelector((state: any) => state.users);
+  const user = users.users.filter((item: any) => item.admin === true);
+  const data: userType | undefined = user.length > 0 ? user[0] : undefined;
   return (
     <GradienBox mywidth="1101px" myheight="173px" myborder="40px">
       <div className='hero-cont'>
         <div className='part1'>
-          <h1 style={{ textTransform: 'capitalize' }}>{'Hello ! ' + data.login + '.'}</h1>
+          <h1 style={{ textTransform: 'capitalize' }}>{data && 'Hello ! ' + data.login + '.'}</h1>
           <p>Ready for a gaming surprise ? Click 'Play' to start a random game and see what awaits !</p>
         </div>
         <a className='playhero' href='/#' >
